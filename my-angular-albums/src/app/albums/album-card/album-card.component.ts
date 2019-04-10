bcf5266a9378b9ca9cb72384e569d8f305a7cb76
mmannan70 <mmannan06@gmail.com>
2019-04-10 15:20:35 -0400

    Chapter 5 first one

--- my-angular-albums/src/app/albums/album-card/album-card.component.ts
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Album } from '../album.model';
+
+
 
 @Component({
   selector: 'app-album-card',
@@ -9,11 +11,13 @@ import { Album } from '../album.model';
 export class AlbumCardComponent implements OnInit {
   @Input()
   album: Album;
+  @Output()
+  albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
   constructor() { }
 
   ngOnInit() {
   }
   showAlbum() {
-    alert('Album selected: ' + this.album.album_name);
+    this.albumClicked.emit(this.album);
 }
 }
--- my-angular-albums/src/app/albums/album.model.ts
+++ my-angular-albums/src/app/albums/album.model.ts
@@ -4,12 +4,19 @@ export interface Album {
   album_name: string;
   genre: string;
   price: number;
-  currency: string;
+  currency?: string;
   on_sale: boolean;
   year: number;
   release_date: string;
   recording_location: string;
   duration: string;
-  URL: string;
+  url: string;
+  tracks: Track[];
 }
 
+export interface Track {
+  id: number;
+  track_number: number;
+  title: string;
+  length: string;
+}
