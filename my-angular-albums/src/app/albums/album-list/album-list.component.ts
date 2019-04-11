c9c9ece4b3d15f592f7e02ed7759eb40d6711a90
mmannan70 <mmannan06@gmail.com>
2019-04-11 15:15:12 -0400

    Updated

--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { ALBUMS } from '../albums.data';
+import { AlbumService } from '../shared/album.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-album-list',
   templateUrl: './album-list.component.html',
   styleUrls: ['./album-list.component.css']
 })
 export class AlbumListComponent implements OnInit {
-  albumsArray: Album[];
-  constructor() { }
+ // albumsArray: Album[];
+
+ albums: Album[];
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit(): void {
-    this.albumsArray = this.albumsArray = ALBUMS;
+   // this.albumsArray = this.albumsArray = ALBUMS;
+    this.getAlbums();
    }
 
   parentFunctionHandler(album) {
     alert('Album ' + album.album_name + ' was sent from the album card component');
 }
+getAlbums() {
+  this.albumService.getAlbums()
+    .subscribe(
+        albums => this.albums = albums,
+        error => console.log("Error: ", error));
 }
+}
+
--- my-angular-albums/src/app/app.module.ts
+++ my-angular-albums/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumListComponent } from './albums/album-list/album-list.component';
 import { AlbumCardComponent } from './albums/album-card/album-card.component';
+import { ReverseStrPipe } from './reverse-str.pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlbumListComponent,
-    AlbumCardComponent
+    AlbumCardComponent,
+    ReverseStrPipe
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
